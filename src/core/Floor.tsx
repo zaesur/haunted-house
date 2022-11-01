965b0f4e075abe30bc84c91a51ecc81292d08fc0
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-02 00:32:40 +0100

    Add textures to scene

--- src/core/Floor.tsx
+++ src/core/Floor.tsx
@@ -1,18 +1,50 @@
 import { useRef, useEffect, FunctionComponent } from "react";
-import { Mesh } from "three";
+import { Float32BufferAttribute, Mesh, RepeatWrapping } from "three";
+import { useTexture } from "@react-three/drei";
 
 const Floor: FunctionComponent = () => {
   const floorRef = useRef<Mesh>(null!);
 
+  const [
+    grassColorTexture,
+    grassAmbientOcclusionTexture,
+    grassNormalTexture,
+    grassRoughnessTexture
+  ] = useTexture([
+    "/textures/grass/color.jpg",
+    "/textures/grass/ambientOcclusion.jpg",
+    "/textures/grass/normal.jpg",
+    "/textures/grass/roughness.jpg",
+  ]).map(texture => {
+    texture.repeat.set(8, 8);
+    texture.wrapS = RepeatWrapping;
+    texture.wrapT = RepeatWrapping;
+    return texture;
+  });
+
   useEffect(() => {
     floorRef.current.rotation.x = -Math.PI * 0.5;
     floorRef.current.position.y = 0;
+
+    floorRef.current.geometry.setAttribute(
+      "uv2",
+      new Float32BufferAttribute(
+        floorRef.current.geometry.attributes.uv.array,
+        2
+      )
+    );
   }, []);
 
   return (
     <mesh receiveShadow ref={floorRef}>
       <planeGeometry args={[20, 20]} />
-      <meshStandardMaterial color="#a9c388" />
+      <meshStandardMaterial
+        color="#a9c388"
+        map={grassColorTexture}
+        aoMap={grassAmbientOcclusionTexture}
+        normalMap={grassNormalTexture}
+        roughnessMap={grassRoughnessTexture}
+      />
     </mesh>
   );
 };
--- src/core/House.tsx
+++ src/core/House.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useRef, FunctionComponent, ReactNode } from "react";
-import { Mesh, MeshStandardMaterial, SphereGeometry } from "three";
+import { useTexture } from "@react-three/drei";
+import {
+  Float32BufferAttribute,
+  Mesh,
+  MeshStandardMaterial,
+  SphereGeometry,
+} from "three";
 
 const bushGeometry = new SphereGeometry(1, 16, 16);
 const bushMaterial = new MeshStandardMaterial({ color: "#89c854" });
@@ -35,10 +41,36 @@ const bushes: Array<BushProps> = [
 ];
 
 interface HouseProps {
-    children?: ReactNode
+  children?: ReactNode;
 }
 
 const House: FunctionComponent<HouseProps> = ({ children }) => {
+  const [
+    doorColorTexture,
+    doorAlphaTexture,
+    doorAmbientOcclusionTexture,
+    doorHeightTexture,
+    doorNormalTexture,
+    doorMetalnessTexture,
+    doorRoughnessTexture,
+    bricksColorTexture,
+    bricksAmbientOcclusionTexture,
+    bricksNormalTexture,
+    bricksRoughnessTexture,
+  ] = useTexture([
+    "/textures/door/color.jpg",
+    "/textures/door/alpha.jpg",
+    "/textures/door/ambientOcclusion.jpg",
+    "/textures/door/height.jpg",
+    "/textures/door/normal.jpg",
+    "/textures/door/metalness.jpg",
+    "/textures/door/roughness.jpg",
+    "/textures/bricks/color.jpg",
+    "/textures/bricks/ambientOcclusion.jpg",
+    "/textures/bricks/normal.jpg",
+    "/textures/bricks/roughness.jpg",
+  ]);
+
   const wallsRef = useRef<Mesh>(null!);
   const wallsWidth = 4;
   const wallsHeight = 2.5;
@@ -48,10 +80,18 @@ const House: FunctionComponent<HouseProps> = ({ children }) => {
   const roofHeight = 1;
 
   const doorRef = useRef<Mesh>(null!);
-  const doorSize = 2;
+  const doorSize = 2.2;
 
   useEffect(() => {
     wallsRef.current.position.y = wallsHeight * 0.5;
+
+    wallsRef.current.geometry.setAttribute(
+      "uv2",
+      new Float32BufferAttribute(
+        wallsRef.current.geometry.attributes.uv.array,
+        2
+      )
+    );
   }, []);
 
   useEffect(() => {
@@ -62,13 +102,27 @@ const House: FunctionComponent<HouseProps> = ({ children }) => {
   useEffect(() => {
     doorRef.current.position.z = wallsWidth / 2 + 0.01;
     doorRef.current.position.y = doorSize / 2;
+
+    doorRef.current.geometry.setAttribute(
+      "uv2",
+      new Float32BufferAttribute(
+        doorRef.current.geometry.attributes.uv.array,
+        2
+      )
+    );
   }, []);
 
   return (
     <group name="house">
       <mesh castShadow ref={wallsRef}>
         <boxGeometry args={[wallsWidth, wallsHeight, wallsWidth]} />
-        <meshStandardMaterial color="#ac8e28" />
+        <meshStandardMaterial
+          color="#ac8e28"
+          map={bricksColorTexture}
+          aoMap={bricksAmbientOcclusionTexture}
+          normalMap={bricksNormalTexture}
+          roughnessMap={bricksRoughnessTexture}
+        />
       </mesh>
 
       <mesh ref={roofRef}>
@@ -77,8 +131,19 @@ const House: FunctionComponent<HouseProps> = ({ children }) => {
       </mesh>
 
       <mesh ref={doorRef}>
-        <planeGeometry args={[doorSize, doorSize]} />
-        <meshStandardMaterial color="#aa7b7b" />
+        <planeGeometry args={[doorSize, doorSize, 100, 100]} />
+        <meshStandardMaterial
+          color="#aa7b7b"
+          transparent
+          map={doorColorTexture}
+          alphaMap={doorAlphaTexture}
+          aoMap={doorAmbientOcclusionTexture}
+          displacementMap={doorHeightTexture}
+          displacementScale={0.1}
+          normalMap={doorNormalTexture}
+          metalnessMap={doorMetalnessTexture}
+          roughnessMap={doorRoughnessTexture}
+        />
       </mesh>
 
       {bushes.map((props, i) => (
